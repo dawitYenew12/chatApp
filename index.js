d541d5a36c7ec4f5528746119ab4df6a2fc3d956
DawitYenew <wizdevaxo.06464@gmail.com>
2024-06-03 19:39:41 +0300

    fixing bugs

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 const PORT = process.env.PORT || 3000;
-const MONGO_URI = MONGO_STRING
+const MONGO_URI = process.env.MONGO_STRING
 
 const upload = multer({ dest: 'uploads/' });  // Configure multer to save files to the 'uploads' folder
 
